a1e78816c0ab536997c2af321d38c40759ddecfd
1evity <124527764+1evity@users.noreply.github.com>
2025-02-09 00:24:02 +0400

    Define convert function and parse HTML to Markdown

--- internal/convert/convert.go
+++ internal/convert/convert.go
@@ -5,3 +5,11 @@ import (
     "github.com/gomarkdown/markdown/html"
     "github.com/gomarkdown/markdown/parser"
 )
+
+// ConvertToMarkdown takes a HTML file and converts it into a Markdown sheet
+func ConvertToMarkdown(htmlContent string) string {
+	// Parse HTML to Markdown
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	p := parser.NewWithExtensions(extensions)
+	tdoc := p.Parse([]byte(htmlContent))
+}
